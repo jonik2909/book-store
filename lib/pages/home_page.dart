5df35be9a4f9fd39e30032c2a62c025527473782
jonik2909 <boronovjonibek@gmail.com>
2024-07-13 10:38:36 +0900

    feat: completed home page viewc

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -21,7 +21,10 @@ class HomePage extends StatelessWidget {
     final Controller controller = Get.put(Controller());
 
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        scrolledUnderElevation: 0,
+        backgroundColor: Colors.white,
         centerTitle: false,
         title: Text(
           'Book Store',
@@ -41,83 +44,136 @@ class HomePage extends StatelessWidget {
         ],
       ),
       drawer: Drawer(),
-      body: Column(
-        children: [
-          SizedBox(height: 30),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: SizedBox(
-              height: 40,
-              child: ListView.separated(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: _categories.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () => controller.changeCategory(index),
-                      child: Obx(() => CategoryCard(
-                            name: _categories[index],
-                            selected:
-                                index == controller.selectedCategory.value,
-                          )),
-                    );
-                  },
-                  separatorBuilder: (context, index) {
-                    return SizedBox(width: 10);
-                  }),
-            ),
-          ),
-          SizedBox(height: 25),
-          Container(
-            height: 50,
-            color: Color(0xffF8F9FA),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Best selling books",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        "See all",
-                        style: TextStyle(
-                          color: Color(0xffEB5757),
-                          fontSize: 13,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                      SizedBox(width: 8),
-                      Icon(
-                        Icons.arrow_forward_ios,
-                        size: 12,
-                        color: Color(0xffEB5757),
-                      ),
-                    ],
-                  )
-                ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 30),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: SizedBox(
+                height: 40,
+                child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: _categories.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () => controller.changeCategory(index),
+                        child: Obx(() => CategoryCard(
+                              name: _categories[index],
+                              selected:
+                                  index == controller.selectedCategory.value,
+                            )),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return SizedBox(width: 10);
+                    }),
               ),
             ),
-          ),
-          SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: SizedBox(
-              height: 310,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 10,
-                  itemBuilder: (context, index) {
-                    return BookCard();
-                  }),
+            SizedBox(height: 25),
+            Container(
+              height: 50,
+              color: Color(0xffF8F9FA),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Best selling books",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "See all",
+                          style: TextStyle(
+                            color: Color(0xffEB5757),
+                            fontSize: 13,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        SizedBox(width: 8),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          size: 12,
+                          color: Color(0xffEB5757),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
             ),
-          )
-        ],
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: SizedBox(
+                height: 310,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 10,
+                    itemBuilder: (context, index) {
+                      return BookCard();
+                    }),
+              ),
+            ),
+            SizedBox(height: 25),
+            Container(
+              height: 50,
+              color: Color(0xffF8F9FA),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Trending Now",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "See all",
+                          style: TextStyle(
+                            color: Color(0xffEB5757),
+                            fontSize: 13,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        SizedBox(width: 8),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          size: 12,
+                          color: Color(0xffEB5757),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: SizedBox(
+                height: 310,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 10,
+                    itemBuilder: (context, index) {
+                      return BookCard();
+                    }),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
