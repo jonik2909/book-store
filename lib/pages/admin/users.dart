0043fb2d097a115aba1750ccf7ae659a6f6f6b3f
jonik2909 <boronovjonibek@gmail.com>
2024-07-16 07:42:39 +0900

    feat: completed admin users view

--- lib/pages/admin/users.dart
+++ lib/pages/admin/users.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class Users extends StatelessWidget {
@@ -6,9 +8,39 @@ class Users extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text("USERS"),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        scrolledUnderElevation: 0,
+        backgroundColor: Colors.white,
+        centerTitle: false,
+        title: Text(
+          'Users list',
+          style: TextStyle(fontWeight: FontWeight.w800),
+        ),
+      ),
+      body: Visibility(
+        // visible: items.isNotEmpty,
+        replacement: Center(
+          child: Text(
+            'No Users',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ),
+        child: ListView.builder(
+            itemCount: 10,
+            padding: EdgeInsets.all(8),
+            itemBuilder: (context, index) {
+              return Card(
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Text('${index + 1}'),
+                  ),
+                  title: Text("item['title']"),
+                  subtitle: Text("item['description']"),
+                  trailing: Icon(Icons.delete),
+                ),
+              );
+            }),
       ),
     );
   }
