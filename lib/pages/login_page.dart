26654503998c92c35a061b84616e7cdc82c33ddb
jonik2909 <boronovjonibek@gmail.com>
2024-12-23 11:46:24 +0900

    fix: modify view join pages

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -16,130 +16,133 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: EdgeInsets.all(33),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('lib/assets/logo_red.png'),
-                SizedBox(height: 50),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color.fromRGBO(0, 0, 0, 0.04),
-                        spreadRadius: 5,
-                        blurRadius: 10,
-                        offset: Offset(0, 1), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                  child: TextField(
-                    controller: usernameController,
-                    autofocus: true,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      hintText: 'Username',
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                        color: Color(0xff8E8E93),
-                      )),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 24),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color.fromRGBO(0, 0, 0, 0.04),
-                        spreadRadius: 5,
-                        blurRadius: 10,
-                        offset: Offset(0, 1), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                  child: TextField(
-                    controller: passwordController,
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      hintText: 'Password',
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                        color: Color(0xff8E8E93),
-                      )),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Obx(() => Text(
-                      memberController.loginErrorMessage.value,
-                      style: TextStyle(color: Colors.red),
-                    )),
-                SizedBox(height: 30),
-                SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      memberController.login(
-                        usernameController.text,
-                        passwordController.text,
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xffEB5757),
-                      shape: RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.circular(10), // Border radius
-                      ),
-                    ),
-                    child: Text(
-                      'Log In',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 24),
-                GestureDetector(
-                  onTap: () => Get.to(SignupPage()),
-                  child: RichText(
-                    text: TextSpan(
-                      text: "Don't have an account yet? ",
-                      style: TextStyle(
-                          color: Color(0xff8E8E93),
-                          fontWeight: FontWeight.w400),
-                      children: [
-                        TextSpan(
-                          text: 'Sign up here',
-                          style: TextStyle(
-                            color: Color(0xff8E8E93),
-                            fontWeight: FontWeight.w700,
-                          ),
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.all(30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('lib/assets/logo_red.png'),
+                  SizedBox(height: 50),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromRGBO(0, 0, 0, 0.04),
+                          spreadRadius: 5,
+                          blurRadius: 10,
+                          offset: Offset(0, 1), // changes position of shadow
                         ),
                       ],
                     ),
+                    child: TextField(
+                      controller: usernameController,
+                      autofocus: true,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Username',
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Color(0xff8E8E93),
+                        )),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                    ),
                   ),
-                )
-              ],
+                  SizedBox(height: 24),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromRGBO(0, 0, 0, 0.04),
+                          spreadRadius: 5,
+                          blurRadius: 10,
+                          offset: Offset(0, 1), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: TextField(
+                      controller: passwordController,
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Password',
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Color(0xff8E8E93),
+                        )),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Obx(() => Text(
+                        memberController.loginErrorMessage.value,
+                        style: TextStyle(color: Colors.red),
+                      )),
+                  SizedBox(height: 30),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        memberController.login(
+                          usernameController.text,
+                          passwordController.text,
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xffEB5757),
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(10), // Border radius
+                        ),
+                      ),
+                      child: Text(
+                        'Log In',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 24),
+                  GestureDetector(
+                    onTap: () => Get.to(SignupPage()),
+                    child: RichText(
+                      text: TextSpan(
+                        text: "Don't have an account yet? ",
+                        style: TextStyle(
+                            color: Color(0xff8E8E93),
+                            fontWeight: FontWeight.w400),
+                        children: [
+                          TextSpan(
+                            text: 'Sign up here',
+                            style: TextStyle(
+                              color: Color(0xff8E8E93),
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -18,160 +18,163 @@ class SignupPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: EdgeInsets.all(33),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('lib/assets/logo_red.png'),
-                SizedBox(height: 50),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color.fromRGBO(0, 0, 0, 0.04),
-                        spreadRadius: 5,
-                        blurRadius: 10,
-                        offset: Offset(0, 1), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                  child: TextField(
-                    controller: nickController,
-                    autofocus: true,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      hintText: 'Username',
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                        color: Color(0xff8E8E93),
-                      )),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 24),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color.fromRGBO(0, 0, 0, 0.04),
-                        spreadRadius: 5,
-                        blurRadius: 10,
-                        offset: Offset(0, 1), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                  child: TextField(
-                    controller: emailController,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      hintText: 'Email',
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                        color: Color(0xff8E8E93),
-                      )),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 24),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color.fromRGBO(0, 0, 0, 0.04),
-                        spreadRadius: 5,
-                        blurRadius: 10,
-                        offset: Offset(0, 1), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                  child: TextField(
-                    controller: passwordController,
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      hintText: 'Password',
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                        color: Color(0xff8E8E93),
-                      )),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Obx(() => Text(
-                      memberController.signupErrorMessage.value,
-                      style: TextStyle(color: Colors.red),
-                    )),
-                SizedBox(height: 30),
-                SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      memberController.signup(
-                        nickController.text,
-                        emailController.text,
-                        passwordController.text,
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xffEB5757),
-                      shape: RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.circular(10), // Border radius
-                      ),
-                    ),
-                    child: Text(
-                      'Create Account',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 24),
-                GestureDetector(
-                  onTap: () => Get.to(LoginPage()),
-                  child: RichText(
-                    text: TextSpan(
-                      text: "Alredy have an account? ",
-                      style: TextStyle(
-                          color: Color(0xff8E8E93),
-                          fontWeight: FontWeight.w400),
-                      children: [
-                        TextSpan(
-                          text: 'Log in here',
-                          style: TextStyle(
-                            color: Color(0xff8E8E93),
-                            fontWeight: FontWeight.w700,
-                          ),
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.all(33),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('lib/assets/logo_red.png'),
+                  SizedBox(height: 50),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromRGBO(0, 0, 0, 0.04),
+                          spreadRadius: 5,
+                          blurRadius: 10,
+                          offset: Offset(0, 1), // changes position of shadow
                         ),
                       ],
                     ),
+                    child: TextField(
+                      controller: nickController,
+                      autofocus: true,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Username',
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Color(0xff8E8E93),
+                        )),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                    ),
                   ),
-                )
-              ],
+                  SizedBox(height: 24),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromRGBO(0, 0, 0, 0.04),
+                          spreadRadius: 5,
+                          blurRadius: 10,
+                          offset: Offset(0, 1), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Email',
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Color(0xff8E8E93),
+                        )),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 24),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromRGBO(0, 0, 0, 0.04),
+                          spreadRadius: 5,
+                          blurRadius: 10,
+                          offset: Offset(0, 1), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: TextField(
+                      controller: passwordController,
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Password',
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Color(0xff8E8E93),
+                        )),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Obx(() => Text(
+                        memberController.signupErrorMessage.value,
+                        style: TextStyle(color: Colors.red),
+                      )),
+                  SizedBox(height: 30),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        memberController.signup(
+                          nickController.text,
+                          emailController.text,
+                          passwordController.text,
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xffEB5757),
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(10), // Border radius
+                        ),
+                      ),
+                      child: Text(
+                        'Create Account',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 24),
+                  GestureDetector(
+                    onTap: () => Get.to(LoginPage()),
+                    child: RichText(
+                      text: TextSpan(
+                        text: "Alredy have an account? ",
+                        style: TextStyle(
+                            color: Color(0xff8E8E93),
+                            fontWeight: FontWeight.w400),
+                        children: [
+                          TextSpan(
+                            text: 'Log in here',
+                            style: TextStyle(
+                              color: Color(0xff8E8E93),
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
