3fdc4c4d3765044686f3afdde4b87b7ae3eeb348
jonik2909 <boronovjonibek@gmail.com>
2025-01-22 17:30:38 +0900

    fix: modify authors page

--- lib/pages/authors/authors_page.dart
+++ lib/pages/authors/authors_page.dart
@@ -12,14 +12,20 @@ class AuthorsPage extends StatelessWidget {
 
   final MemberController memberController = Get.put(MemberController());
 
+  Future<void> refreshData() async {
+    // Convert the void return type to Future<void>
+    await memberController.getAuthorList(
+      order: 'createdAt',
+      page: 1,
+      limit: 100,
+      memberType: MemberType.AUTHOR,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      memberController.getAuthorList(
-          order: 'createdAt',
-          page: 1,
-          limit: 100,
-          memberType: MemberType.AUTHOR);
+      refreshData();
     });
 
     return Scaffold(
@@ -60,61 +66,65 @@ class AuthorsPage extends StatelessWidget {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 30.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 30),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  children: [
-                    Obx(() {
-                      if (memberController.isLoading.value) {
-                        return Center(child: CircularProgressIndicator());
-                      }
+      body: RefreshIndicator(
+        onRefresh: () => refreshData(),
+        child: ListView(physics: AlwaysScrollableScrollPhysics(), children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: [
+                      Obx(() {
+                        if (memberController.isLoading.value) {
+                          return Center(child: CircularProgressIndicator());
+                        }
 
-                      if (memberController.authorList.isEmpty) {
-                        return Center(
-                          child: Text(
-                            'No data found!',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.grey[600],
+                        if (memberController.authorList.isEmpty) {
+                          return Center(
+                            child: Text(
+                              'No data found!',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.grey[600],
+                              ),
                             ),
-                          ),
+                          );
+                        }
+                        return Wrap(
+                          direction: Axis.horizontal,
+                          alignment: WrapAlignment.start,
+                          spacing: 10,
+                          runSpacing: 20,
+                          children: memberController.authorList.map((author) {
+                            return AuthorCard(
+                                memberNick: author.memberNick,
+                                memberEmail: author.memberEmail,
+                                memberImage: author.memberImage,
+                                memberViews: author.memberViews,
+                                onTap: () => Get.to(() => AuthorDetailPage(),
+                                        arguments: {'memberId': author.id})
+                                    ?.then((_) =>
+                                        memberController.getAuthorList(
+                                            order: 'createdAt',
+                                            page: 1,
+                                            limit: 100,
+                                            memberType: MemberType.AUTHOR)));
+                          }).toList(),
                         );
-                      }
-                      return Wrap(
-                        direction: Axis.horizontal,
-                        alignment: WrapAlignment.start,
-                        spacing: 10,
-                        runSpacing: 20,
-                        children: memberController.authorList.map((author) {
-                          return AuthorCard(
-                              memberNick: author.memberNick,
-                              memberEmail: author.memberEmail,
-                              memberImage: author.memberImage,
-                              memberViews: author.memberViews,
-                              onTap: () => Get.to(() => AuthorDetailPage(),
-                                      arguments: {'memberId': author.id})
-                                  ?.then((_) => memberController.getAuthorList(
-                                      order: 'createdAt',
-                                      page: 1,
-                                      limit: 100,
-                                      memberType: MemberType.AUTHOR)));
-                        }).toList(),
-                      );
-                    })
-                  ],
-                ),
-              )
-            ],
+                      })
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
+        ]),
       ),
     );
   }
