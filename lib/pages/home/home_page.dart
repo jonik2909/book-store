cf731ee9d1550991e46a7e87712caed656a1e99e
jonik2909 <boronovjonibek@gmail.com>
2025-04-03 14:08:42 +0900

    fix: refactoring

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:book_store/components/VideoSection.dart';
-import 'package:book_store/components/AuthorHomeCard.dart';
-import 'package:book_store/components/BookCard.dart';
-import 'package:book_store/components/EventsSection.dart';
+import 'package:book_store/components/video_section.dart';
+import 'package:book_store/components/author_home_card.dart';
+import 'package:book_store/components/book_card.dart';
+import 'package:book_store/components/events_section.dart';
 import 'package:book_store/controller/member.controller.dart';
 import 'package:book_store/models/Member.dart';
 import 'package:book_store/pages/authors/chosen_author_page.dart';
