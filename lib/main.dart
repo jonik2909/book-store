b49581b8d89380b9da42bcd2c7eab30c5199c6cc
jonik2909 <boronovjonibek@gmail.com>
2025-06-25 15:34:23 +0900

    fix: refactor main dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,12 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
+  // Flutter platformasi to‘liq ishga tushishini kafolatlash uchun kerak,
+  // ayniqsa async kodlar (await) oldidan chaqiriladi.
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
-  runApp(GetMaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MyApp(),
-  ));
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,13 +18,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        textTheme: GoogleFonts.montserratTextTheme(
-          Theme.of(context).textTheme,
-        ),
-      ),
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.montserratTextTheme(), // ✅ context kerak emas
+      ),
       home: MainPage(),
     );
   }
